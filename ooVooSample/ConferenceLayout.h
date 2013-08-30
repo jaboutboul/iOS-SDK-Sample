f2fe2e7c0d62761a38e2213e83df6839199c51ba
ooVoo Developers <github@oovoo.com>
2013-08-30 11:31:38 -0400

    Updated licensing information in file headers

--- ooVooSample/ConferenceLayout.h
+++ ooVooSample/ConferenceLayout.h
@@ -3,7 +3,7 @@
 // 
 // Created by ooVoo on July 22, 2013
 //
-// © 2013 ooVoo, LLC.  Used under license. 
+// © 2013 ooVoo, LLC.  License under Apache 2.0 license. http://www.apache.org/licenses/LICENSE-2.0.html 
 //
 
 @interface ConferenceLayout : UICollectionViewFlowLayout
